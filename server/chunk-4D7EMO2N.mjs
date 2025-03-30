84288fb71737d0d9eb6d8a0e3334771cb6c3d19f
neeraj48 <neerajkumar92july@gmail.com>
2025-03-30 18:01:27 +0530

    Auto-generated commit

---
+++ server/chunk-4D7EMO2N.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Cb as p,Gb as s,ba as n,kb as o,lb as a}from"./chunk-WOW6A6OQ.mjs";import"./chunk-5XUXGTUW.mjs";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-not-found"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,r){e&1&&(o(0,"p"),p(1,"not-found works!"),a())}})};export{i as NotFoundComponent};
