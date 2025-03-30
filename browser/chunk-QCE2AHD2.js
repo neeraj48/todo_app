ede83e0dc828c1c8755a43b932444b82adae8ee7
neeraj48 <neerajkumar92july@gmail.com>
2025-03-30 17:24:13 +0530

    Auto-generated commit

---
+++ browser/chunk-QCE2AHD2.js
@@ -0,0 +1,1 @@
+import{Db as p,Hb as s,ea as n,lb as o,mb as a}from"./chunk-XIDA3FLF.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-not-found"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,r){e&1&&(o(0,"p"),p(1,"not-found works!"),a())}})};export{i as NotFoundComponent};
